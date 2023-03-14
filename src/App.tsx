df72e66911276de99ae7d9e868a9d0f127c3ec12
Palocz Istvan <istvan@palocz.hu>
2023-03-14 04:52:54 +0100

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav>
+    <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">About</a></li>
+    </ul>
+    </nav>
+    <div id="detail">
+      <Outlet />
     </div>
+    </>
   );
 }
 
---
+++ src/components/About.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+class About extends React.Component <{}> {
+
+render() {
+    return (
+        <div>About</div>
+    );
+}
+}
+
+export default About;
